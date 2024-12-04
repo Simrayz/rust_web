9e89a76be17618b25706233b69a29627a47f0ddf
Simen Abelsen <simen.nordbo.abelsen@brilliant.no>
2024-12-04 01:07:00 +0100

    💬 Reveal the juith

--- alpine_html/src/routes/home.rs
+++ alpine_html/src/routes/home.rs
@@ -1,11 +1,3 @@
-/*
-<div x-data="{ open: false }">
-    <button @click="open = ! open">Toggle</button>
-
-    <div x-show="open" @click.outside="open = false">Contents...</div>
-</div>
-*/
-
 use axum::response::IntoResponse;
 use rust_html::{rhtml, Template, TemplateGroup};
 
@@ -17,8 +9,10 @@ pub async fn home_page() -> impl IntoResponse {
         <div class="text-gray-900 dark:text-gray-100 space-y-6">
             <h1 class="text-3xl">Home</h1>
             <div x-data="{{ open: false }}" class="space-y-2">
-                <button @click="open = ! open" type="button" class="{ButtonColor::Primary}">Reveal secrets</button>
-                <div x-show="open" @click.outside="open = false" class="bg-gray-200 dark:bg-gray-800 rounded-md p-4">Contents...</div>
+                <button @click="open = ! open" type="button" class="{ButtonColor::Primary}">Do you want the juith? 🧃</button>
+                <blockquote x-show="open" @click.outside="open = false" class="p-4 my-4 bg-gray-50 border-l-4 border-gray-300 dark:border-gray-500 dark:bg-gray-800">
+                    The truth is that the universe has been answering you all of your life, but you cannot receive the answers unless you are awake.
+                </blockquote>
             </div>
             <div class="space-y-4">
                 <h3 class="text-lg">Some interesting boxes</h3>
